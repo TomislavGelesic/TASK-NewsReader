faf428e1ed72fca06665274f5174a81ebfb2c242
Tomislav Gelesic <gelesic.tomislav@gmail.com>
2020-10-21 12:51:56 +0200

    feat: spinner, error alert on API error

--- TASK-NewsReader/MainScreen/NewsTableViewController.swift
+++ TASK-NewsReader/MainScreen/NewsTableViewController.swift
@@ -13,20 +13,40 @@ class NewsTableViewController: UITableViewController {
     
     var articles = [NewsAPI.Article]()
     
+    var timer: Timer?
+    
+    let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        spinner.style = .large
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    
+    
     //MARK: Life-cycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
         view.backgroundColor = .white
+        view.addSubview(spinner)
+        
         navigationItem.title = "News reader"
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
         self.navigationController?.navigationBar.barTintColor = .blue
-               
+        
         tableView.register(NewsTableViewCell.self, forCellReuseIdentifier: "cellId")
         tableView.rowHeight = 65
+        
+        self.timer = Timer.scheduledTimer(timeInterval: 5*60, target: self, selector: #selector(refreshNews), userInfo: nil, repeats: true)
+        
+        spinnerConstraints()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         fetchData()
     }
     
+    //MARK: UITableView OVERRIDES
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.articles.count
     }
@@ -42,7 +62,7 @@ class NewsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let detailScreen = DetailScreenViewController()
         detailScreen.title = self.articles[indexPath.row].title
-//        detailScreen.navigationController?.navigationBar.tintColor = .blue
+        //        detailScreen.navigationController?.navigationBar.tintColor = .blue
         detailScreen.imageViewDetails.image = UIImage(url: URL(string: articles[indexPath.row].urlToImage))
         detailScreen.titleLabelDetails.text = articles[indexPath.row].title
         detailScreen.textLabelDetails.text = articles[indexPath.row].description
@@ -55,41 +75,60 @@ class NewsTableViewController: UITableViewController {
 extension NewsTableViewController {
     
     private func fetchData() {
+        
         guard let url = URL(string: "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=d5017336d77b4bd98755d5c62d353a04") else { return }
-    
+        
+        spinner.startAnimating()
+        
         let session = URLSession.shared
         session.dataTask(with: url) { (data, response, error) in
             
             if let data = data {
                 do{
                     let json = try JSONDecoder().decode(NewsAPI.self, from: data)
-//                    let json = try JSONSerialization.jsonObject(with: data, options: [])
-//                    print(json.articles[0].title)
+                    //                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    //                    print(json.articles[0].title)
                     for article in json.articles{
                         self.articles.append(article)
                     }
                 } catch {
-                    print("im here")
-                    print(error)
+                    let alert = UIAlertController(title: "Error", message: "Ups, error occured!", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                    DispatchQueue.main.async {
+                        self.present(alert, animated: true, completion: nil)
+                        self.fetchData()
+                    }
+                    
                 }
             }
             DispatchQueue.main.async {
                 self.tableView.reloadData()
+                self.spinner.stopAnimating()
             }
         }.resume()
     }
+    
+    @objc func refreshNews() {
+        fetchData()
+    }
+    
+    func spinnerConstraints () {
+        spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
 }
+
 
 // MARK: extension UIImage from URL
 extension UIImage {
-  convenience init?(url: URL?) {
-    guard let url = url else { return nil }
-            
-    do {
-      self.init(data: try Data(contentsOf: url))
-    } catch {
-      print("Cannot load image from url: \(url) with error: \(error)")
-      return nil
+    convenience init?(url: URL?) {
+        guard let url = url else { return nil }
+        
+        do {
+            self.init(data: try Data(contentsOf: url))
+        } catch {
+            print("Cannot load image from url: \(url) with error: \(error)")
+            return nil
+        }
     }
-  }
 }
